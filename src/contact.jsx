7b41a23c36d386ec0da32ea0674610526b0b84b3
Surendhar242002 <justileremo@gmail.com>
2026-02-04 09:30:41 +0530

    change file name contact

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Contact.css";
-
+    
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
